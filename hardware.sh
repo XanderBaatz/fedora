d7f9608d9c53e09120f7068e7ad339dc477eb436
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-09 19:37:01 +0100

    Update hardware.sh

--- hardware.sh
+++ hardware.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#Required repositories
+sudo dnf install -y \
+https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
 #Fix Bluetooth and Wi-Fi
-sudo dnf group install "Hardware Support"
+sudo dnf group install -y "Hardware Support"
 
