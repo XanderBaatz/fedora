f68671e6b1f412481cdbf4f2ef3ffcb3bc4a9a54
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-10 15:42:31 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -2,7 +2,10 @@
 # wget -qO- https://git.io/XXXXX | sh
 
 #install base packages
-sudo dnf install -y gdm gnome-shell gnome-terminal nautilus
+sudo dnf install -y gdm gnome-shell gnome-terminal nautilus \
+chrome-gnome-shell gnome-tweaks @development-tools \
+gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer file-roller-nautilus \
+gnome-calculator gnome-system-monitor gedit
 
 # Enable gdm display manager and enable graphical desktop
 sudo systemctl enable gdm
