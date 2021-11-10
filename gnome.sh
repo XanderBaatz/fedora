bd794643b471aeaac68b88803e4f0dc40f4c848a
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-10 13:08:58 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 # wget -qO- https://git.io/XXXXX | sh
 
-sudo dnf install -y 
+#install base packages
+sudo dnf install -y gdm gnome-shell gnome-terminal nautilus
+
+#
+
+#systemctl enable gdm; # Enable login using graphical interface
+#systemctl set-default graphical.target; # Boot to graphical interface as default
