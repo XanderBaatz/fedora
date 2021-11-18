41000c4081a7aa9dc36c92ee844c81d782e03492
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-18 13:13:21 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -2,12 +2,7 @@
 # wget -qO- https://git.io/JXD3d | sh
 
 #install base packages
-sudo dnf install -y gdm gnome-shell gnome-terminal nautilus \
-chrome-gnome-shell gnome-tweaks @development-tools \
-gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer file-roller-nautilus \
-gnome-calculator gnome-system-monitor gnome-disk-utility gedit google-noto-emoji-color-fonts \
-gnome-shell-extension-appindicator gnome-shell-extension-pop-shell xprop \
-papirus-icon-theme
+sudo dnf install -y gdm gnome-shell gnome-terminal nautilus
 
 # Enable gdm display manager and enable graphical desktop
 sudo systemctl enable gdm
