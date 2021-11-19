ecde3b6664bc69c51cb42b750c68b42ae861bab0
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-19 20:51:27 +0100

    Update gnome-extra.sh

--- gnome-extra.sh
+++ gnome-extra.sh
@@ -6,8 +6,12 @@ sudo dnf install -y \
 chrome-gnome-shell gnome-tweaks @development-tools \
 gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer file-roller-nautilus \
 gnome-calculator gnome-system-monitor gnome-disk-utility gedit google-noto-emoji-color-fonts \
-gnome-shell-extension-appindicator gnome-shell-extension-pop-shell gnome-shell-extension-gsconnect xprop \
+gnome-shell-extension-appindicator gnome-shell-extension-gsconnect
 #papirus-icon-theme \
+
+#tiling window manager extension with working shortcuts
+sudo dnf install -y -setopt=install_weak_deps=False \
+gnome-shell-extension-pop-shell xprop
 
 #Open gsconnect ports in firewalld to enable connection
 #sudo firewall-cmd --zone=public --permanent --add-port=1714-1764/tcp
