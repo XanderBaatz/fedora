83112daf5908c5a2542eebe98655f52e59cf8651
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-19 18:34:54 +0100

    Update nvidia-proprietary.sh

--- nvidia-proprietary.sh
+++ nvidia-proprietary.sh
@@ -14,22 +14,22 @@ sudo cp /etc/dnf/dnf.conf /etc/dnf/dnf.conf.bak
 
 if [ $(grep -q -P '^exclude=' /etc/dnf/dnf.conf; echo $?) != "0" ]; then
   sed "/\[main\]/a exclude=${pkgs}" /etc/dnf/dnf.conf | sudo tee /etc/dnf/dnf.conf
-else
+elif [ $(grep -q -P "^exclude=.*${pkgs}.*$" /etc/dnf/dnf.conf; echo $?) != "0" ]; then
   sed "/exclude=/s/$/ ${pkgs}/" /etc/dnf/dnf.conf | sudo tee /etc/dnf/dnf.conf
 fi
 
 #blacklist the nouveau module as a module and in dracut
-#sudo echo 'blacklist nouveau' > /etc/modprobe.d/blacklist-nouveau.conf
-#sudo echo 'omit_drivers+=" nouveau "' > /etc/dracut.conf.d/blacklist-nouveau.conf
+sudo echo 'blacklist nouveau' > /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo 'omit_drivers+=" nouveau "' > /etc/dracut.conf.d/blacklist-nouveau.conf
 
 #backup the current initramfs
-#sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
+sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
 
 #regenerate the current initramfs
-#sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r)
+sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r)
 
-#
-#sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+#update grub config
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
-#install nvidia driver and optional packages to enable cuda/nvenc
+#install proprietary nvidia driver and optional packages to enable cuda/nvenc
 sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
