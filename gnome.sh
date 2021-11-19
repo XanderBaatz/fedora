bc7743f40b7242045c874d152877a575042b8969
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-19 20:09:17 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # wget -qO- https://git.io/JXD3d | sh
 
+# https://www.certdepot.net/rhel7-get-started-package-groups/
+
 #install base packages
-sudo dnf install -y gdm gnome-shell gnome-terminal nautilus
+sudo dnf install -y --setopt=group_package_types=mandatory @critical-path-gnome
+#sudo dnf install -y gdm gnome-shell gnome-terminal nautilus
 #sudo dnf install -y @critical-path-gnome
 
 # Enable gdm display manager and enable graphical desktop
