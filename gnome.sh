055fb3cfc7ee86c0a1e714580c284ebcb5bb2638
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-20 00:18:34 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -11,15 +11,3 @@ sudo dnf install -y --setopt=group_package_types=mandatory @critical-path-gnome
 # Enable gdm display manager and enable graphical desktop
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
-
-#Remove gnome-tour? (check workspace switching)
-
-#Enable installed extensions on next boot
-
-#Disable mouse accel for all users using gsettings
-
-#Download and set wallpaper
-
-#Make adwaita-dark the theme for all users (gsettings)
-
-#Make papirus icon theme the default for all users
