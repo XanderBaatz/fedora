fe2502072cba1005a3964ebbed348a1c006f34d3
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-28 00:00:24 +0100

    Update laptop-battery.sh

--- laptop-battery.sh
+++ laptop-battery.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 # wget -qO- https://git.io/XXXXX | sh
 
-#exit if network-manager isn't installed
+#disable power-profiles-daemon if installed (tlp and ppd can't run simultaneously)
 if [ $(dnf list --installed | grep -q -P "power-profiles-daemon"; echo $?) = "0" ]; then
   sudo systemctl mask power-profiles-daemon.service
 fi
 
+#install tlp and tlp-rdw
+sudo dnf install -y tlp tlp-rdw
+
+#start tlp without rebooting
+sudo tlp start
+
+#disable rfkill to avoid radio switching conflict
+sudo systemctl mask systemd-rfkill.socket
+sudo systemctl mask systemd-rfkill.service
