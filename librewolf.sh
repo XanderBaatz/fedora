6b2b26bc36996ff6d441c319be3d01fd6140161d
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-01 10:48:49 +0000

    Create librewolf.sh

---
+++ librewolf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# wget -qO- https://git.io/XXXXX | sh
+
+# Post install settings and "about:config":
+# * Enable browsing history.
+# * Set "layout.frame_rate" to screen refresh rate.
+# * Set fonts to use system fonts, e.g. on GNOME change it to Cantarell.
+
+sudo flatpak install flathub io.gitlab.librewolf-community
