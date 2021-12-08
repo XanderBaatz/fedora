576e37fee22d8c0db1a6694de25ad8ee4449c113
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-08 12:49:14 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -5,6 +5,11 @@
 # https://forums.fedoraforum.org/showthread.php?324485-steps-After-install
 # https://bluehatrecord.wordpress.com/2015/10/06/the-hidden-dnf-groups-of-the-fedora-repositories/
 
+#check for root privileges
+#if [ "$(whoami)" != "root" ]; then
+#  echo "Root priviliges required to run commands. Would you like to run as root and continue the script? [y/N]"
+#fi
+
 # Check for updates
 sudo sh -c 'dnf --refresh check-upgrade -y && dnf upgrade -y'
 
