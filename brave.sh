09ddd47e1e036b610c981194b2f2b86a3bac4df7
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-09 16:39:14 +0100

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # wget -qO- https://git.io/JXr5G | sh
 
-sudo dnf install dnf-plugins-core -y
+#sudo dnf install dnf-plugins-core -y
 
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+#sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-sudo dnf install brave-browser -y
+#sudo dnf install brave-browser -y
