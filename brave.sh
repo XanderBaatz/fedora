7470b7ea16f335069c74dd1f7cc7c5b8e9bfe65f
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-09 18:22:49 +0100

    Update brave.sh

--- brave.sh
+++ brave.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 # wget -qO- https://git.io/JXr5G | sh
 
+sudo sh -c "cat << EOF > /etc/yum.repos.d/brave-browser.repo
+[brave-browser]
+name=Brave Browser based on Chromium
+baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64/
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+enabled=1
+EOF"
+
+sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/brave-browser.conf
+[brave-browser]
+type=dnf
+EOF"
+
+#[brave-browser-rpm-release.s3.brave.com_x86_64_]
+#name=created by dnf config-manager from https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#enabled=1
+
 #sudo dnf install dnf-plugins-core -y
 
 #sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
