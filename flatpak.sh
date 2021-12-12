4f6f99ddb84b4ca96376bc6c08df0a5853004b60
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-12 12:57:40 +0100

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # wget -qO- https://git.io/XXXXX | sh
 
-#install flatpak
-sudo dnf install -y flatpak
-
+#install flatpak and add flathub repo
+sudo dnf install -y flatpak && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
