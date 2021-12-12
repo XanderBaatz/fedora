6e567d7575db27cf7575ef5d039ffaba985780de
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-12 13:26:05 +0100

    Update rpmfusion.sh

--- rpmfusion.sh
+++ rpmfusion.sh
@@ -4,7 +4,7 @@
 
 #install repositories and third-party repository manager
 sudo dnf install -y \
-fedora-workstation-repositories \
+fedora-third-party \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
