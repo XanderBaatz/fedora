ece56d26d13b3739ebf08ee719f40e24fe385fc1
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-14 15:29:26 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,23 +1,88 @@
 #!/bin/sh
-# wget -qO- https://git.io/JXD3d | sh
-# curl -fsSL https://git.io/JXD3d | sh
 
-# Credits: https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
-#          https://www.certdepot.net/rhel7-get-started-package-groups/
-#          https://www.shellscript.sh/tips/getopt/index.html
+### installer script
 
-set -e
+usage() {
+  echo "Usage: "$0" [ -m | --minimal ]
+  		     [ -n | --nothemes ]
+  		     [ -s | --storeinclude ]
+  		     [ -u | --uninstall ]"
+  exit 2
+}
 
-#check for root privileges
-if [ $(id -u) != 0 ]; then
-  echo ""
-  echo "Root priviliges required to run commands."
-  exec sudo -- "$0" "$@"
+PARSED_ARGUMENTS=$(getopt -a -n "$0" -o mns --long minimal,nothemes,store -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
 fi
 
-# env args
-: "${apps:=true}"
-: "${uninstall:=false}"
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
+  case "$1" in
+    -m | --minimal)       norminst=false     ; shift   ;;
+    -n | --nothemes)      themes=false       ; shift   ;;
+    -s | --storeinclude)  store=true         ; shift   ;;
+    -u | --uninstall)     uninstall=true     ; shift   ;;
+    # -- means the end of the arguments; drop this, and break out of the while loop
+    --) shift; break ;;
+    # If invalid options were passed, then getopt should have reported an error,
+    # which we checked as VALID_ARGUMENTS when getopt was called...
+    *) echo "Unexpected option: $1 - invalid argument."
+       usage ;;
+  esac
+done
+
+
+### package list compiler
+
+pkgs="@critical-path-gnome"
+
+if [ "$store" != "true" ]; then
+  echo "Store will not be installed."
+  dnf_opts="--setopt=group_package_types=mandatory"
+else
+  echo "Store will be installed."
+fi
+
+if [ "$norminst" != "false" ]; then
+  echo "Normal installation."
+  pkgs="$pkgs \
+        @development-tools \
+        nautilus \
+        gnome-extensions-app \
+        chrome-gnome-shell \
+        gnome-tweaks \
+        gnome-terminal-nautilus \
+	xdg-user-dirs \
+	xdg-user-dirs-gtk \
+	ffmpegthumbnailer \
+	file-roller-nautilus \
+	gtkhash-nautilus \
+	gnome-calculator \
+	gnome-system-monitor \
+	gnome-disk-utility \
+	gedit \
+	google-noto-emoji-color-fonts \
+	gnome-shell-extension-appindicator \
+	dbus-x11 \
+	gvfs-goa \
+	"
+else
+  echo "Minimal installation."
+fi
+
+if [ "$themes" != "false" ]; then
+  echo "Themes will be installed."
+  pkgs="$pkgs \
+	papirus-icon-theme \
+	"
+else
+  echo "Themes will not be installed."
+fi
+
+
+# functions
 
 _msg() {
     echo "=>" "$@"
@@ -25,28 +90,23 @@ _msg() {
 
 _uninstall() {
     _msg "Removing packages ..."
-    sudo dnf autoremove -y
+    echo sudo dnf autoremove -y ${pkgs} | sh
 }
 
 _install() {
     _msg "Installing packages ..."
-    sudo dnf install -y
+    echo sudo dnf install -y ${dnf_opts} ${pkgs} #| sh
+    # Enable gdm display manager and enable graphical desktop
+    echo ""
+    echo sudo systemctl enable gdm #| sh
+    echo sudo systemctl set-default graphical.target #| sh
 }
 
-if [ "$apps" = "true" ]; then
-    pkgs=""
-fi
 
-if [ "$uninstall" = "false" ]; then
-    _install "$ICON_THEMES"
+### execution
+
+if [ "$uninstall" != "true" ]; then
+  _install
 else
-    _uninstall "$ICON_THEMES"
+  _uninstall
 fi
-
-#install base packages
-sudo dnf install -y --setopt=group_package_types=mandatory --setopt=exclude=gnome-tour \
-@critical-path-gnome nautilus
-
-# Enable gdm display manager and enable graphical desktop
-sudo systemctl enable gdm
-sudo systemctl set-default graphical.target
