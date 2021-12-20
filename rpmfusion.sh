e2de078f69d74f55fd33fcd4d5f9040db944aa23
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-20 16:09:31 +0100

    Update rpmfusion.sh

--- rpmfusion.sh
+++ rpmfusion.sh
@@ -2,33 +2,32 @@
 # wget -qO- https://git.io/JDtlC | sh
 # curl -fsSL https://git.io/JDtlC | sh
 
-#prompt to select what repos you want enabled
+# prompt to select what repos you want enabled
 # Minimal: only base rpmfusion free and nonfree
 # Recommended: fedora workstation repos + Minimal
 # All: rpmfusion tainted and all of the above
 
-#install repositories and third-party repository manager
+# install repositories and third-party repository manager
 sudo dnf install -y \
 fedora-third-party \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 
-#add rpmfusion entry configs to fedora-third-party
+# add rpmfusion entry configs to fedora-third-party
 sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/rpmfusion.conf
 [rpmfusion-free]
 type=dnf
 
 [rpmfusion-nonfree]
 type=dnf
-
-[rpmfusion-free-tainted]
-type=dnf
-
-[rpmfusion-nonfree-tainted]
-type=dnf
 EOF"
 
-#append tainted repos to fedora-third-party
+# Install rpmfusion tainted repos
+#sudo dnf install -y \
+#rpmfusion-free-release-tainted \
+#rpmfusion-nonfree-release-tainted
+
+# append tainted repos to fedora-third-party
 #sudo sh -c "cat << EOF >> /usr/lib/fedora-third-party/conf.d/rpmfusion.conf
 #[rpmfusion-free-tainted]
 #type=dnf
