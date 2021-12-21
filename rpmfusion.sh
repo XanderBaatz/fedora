f7ae71c6681cc05248e56a7069f8a1743ed30e88
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-21 11:42:51 +0100

    Update rpmfusion.sh

--- rpmfusion.sh
+++ rpmfusion.sh
@@ -2,27 +2,71 @@
 # wget -qO- https://git.io/JDtlC | sh
 # curl -fsSL https://git.io/JDtlC | sh
 
-# prompt to select what repos you want enabled
-# Minimal: only base rpmfusion free and nonfree
-# Recommended: fedora workstation repos + Minimal
-# All: rpmfusion tainted and all of the above
+
+# check for root privileges
+if [ $(id -u) -ne 0 ]; then
+  exec sudo -- "$0" "$@"
+fi
+
+# message
+_msg() {
+    echo "=>" "$@"
+}
+
+
+### installer script
+
+usage() {
+  echo "usage: "$0" [-w | --workstation] [-t | --tainted]"
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n "$0" -o wt --long workstation,tainted -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
+  case "$1" in
+    -w | --workstation)  workstation=true     ; shift   ;;
+    -t | --tainted)      tainted=true         ; shift   ;;
+    #
+    --) shift; break ;;
+    #
+    *) echo "Unexpected option: $1 - invalid argument."
+       usage ;;
+  esac
+done
+
 
 # install repositories and third-party repository manager
-sudo dnf install -y \
-fedora-third-party \
-https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 
-# add rpmfusion entry configs to fedora-third-party
-sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/rpmfusion.conf
-[rpmfusion-free]
-type=dnf
+_normal() {
+  sudo dnf install -y \
+  fedora-third-party \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 
-[rpmfusion-nonfree]
-type=dnf
-EOF"
+  # add rpmfusion entry configs to fedora-third-party
+  sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/rpmfusion.conf
+  [rpmfusion-free]
+  type=dnf
 
-tainted() {
+  [rpmfusion-nonfree]
+  type=dnf
+  EOF"
+}
+
+_workstation() {
+  # Install rpmfusion tainted repos
+  sudo dnf install -y \
+  fedora-workstation-repositories
+}
+
+_tainted() {
   # Install rpmfusion tainted repos
   sudo dnf install -y \
   rpmfusion-free-release-tainted \
@@ -38,5 +82,21 @@ tainted() {
   EOF"
 }
 
-#enable repos
-sudo fedora-third-party enable
+_enable_repos() {
+  _msg "Enabling repos."
+  sudo fedora-third-party enable
+}
+
+_normal
+
+if [ "$workstation" = "true" ]; then
+  _msg "Fedora third party Workstation repos will be installed."
+  _workstation
+fi
+
+if [ "$tainted" = "true" ]; then
+  _msg "RPM Fusion Tainted repos will be installed."
+  _tainted
+fi
+
+_enable_repos
