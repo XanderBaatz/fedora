c9ae64c053d34c5d4488ea7abb9e85c26b3c6d12
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-12-23 11:54:48 +0100

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,6 +2,11 @@
 # wget -qO- https://git.io/XXXXX | sh
 # curl -fsSL https://git.io/XXXXX | sh
 
+#check for root privileges
+if [ $(id -u) != 0 ]; then
+  exec sudo -- "$0" "$@"
+fi
+
 #install flatpak and add flathub repo
 sudo dnf install -y flatpak && \
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
