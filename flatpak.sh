b6f5aa1c43d97fa85aaa30d4b2e3a5e6e4cd362c
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2022-01-03 11:38:55 +0100

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -10,3 +10,12 @@ fi
 #install flatpak and add flathub repo
 sudo dnf install -y flatpak && \
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#add third party entry
+sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/rpmfusion.conf
+[rpmfusion-free]
+type=dnf
+
+[rpmfusion-nonfree]
+type=dnf
+EOF"
