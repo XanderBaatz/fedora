752c07113404548172f80ab3464be1831e0e0793
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2022-01-03 12:59:18 +0100

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -8,16 +8,27 @@ if [ $(id -u) != 0 ]; then
 fi
 
 #install flatpak and add flathub repo
-sudo dnf install -y flatpak && \
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf install -y flatpak
 
-#add third party entry if fedora-third-party is installed
+#add and enable third party entry if fedora-third-party is installed
 fedora_pkgs="fedora-third-party"
+fedoratp_dir="/usr/lib/fedora-third-party/conf.d"
+flathub_url="https://flathub.org/repo/flathub.flatpakrepo"
 
-if [ $(dnf list --installed | grep -q -P "${fedora_pkgs}"; echo $?) = "999" ]; then
-  sudo sh -c "cat << EOF > /usr/lib/fedora-third-party/conf.d/flathub.conf
+if [ $(dnf list --installed | grep -q -P "${fedora_pkgs}"; echo $?) = "0" ]; then
+  # download flathub repo file
+  wget -qO- https://flathub.org/repo/flathub.flatpakrepo
+  
+  # add third party repo
+  sudo sh -c "cat << EOF > ${fedoratp_dir}/flathub.conf
   [flathub]
   type=flatpak
-  flatpakrepo=?
+  flatpakrepo=${flathub_url}
   EOF"
+  
+  # enable third party repos
+  
+else
+  # add flathub repo through flatpak
+  flatpak remote-add --if-not-exists flathub ${flathub_url}
 fi
