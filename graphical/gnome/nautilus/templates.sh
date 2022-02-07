ada64f7ffeb1bb03f7563a19836a4a3047f7dbcc
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2022-02-07 11:51:59 +0100

    Update templates.sh

--- graphical/gnome/nautilus/templates.sh
+++ graphical/gnome/nautilus/templates.sh
@@ -3,11 +3,11 @@
 # wget link
 
 # make template function
-#make_temp (){
-#  cat > ${HOME}/Templates/${1}.sh << EOF
-#  ${2}
-#  EOF
-#}
+make_temp (){
+  cat > ${HOME}/Templates/${1}.sh <<EOF
+${2}
+EOF
+}
 
 # bash script template
 cat > ${HOME}/Templates/script.sh << EOF
